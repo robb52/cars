551deec81db2534e3b33fc86c7c9879b0c7e4969
Rob Brown <brown4homes@yahoo.com>
2019-05-13 15:53:06 -0700

    Added actions to cars controller and views

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,7 +1,35 @@
 class CarsController < ApplicationController
+  before_action :find_car, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @cars = Car.all.order("created_at DESC")
   end
-  
+
+  def show
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+
+    if @car.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:title, :description)
+  end
+
+  def find_car
+    @car = Car.find(params[:id])
+  end
+
 end
